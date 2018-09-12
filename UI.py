c1f0565d0a553d193b87df106f33b79ed862962d
Sheeplib <sheeplib@hotmail.com>
2018-09-12 22:57:27 +0800

    update

--- UI.py
+++ UI.py
@@ -7,23 +7,58 @@ Created on Wed Sep 12 00:22:37 2018
 
 try:
     import tkinter as tk
+    from tkinter import filedialog
 except Importerror:
     import Tkinter as tk
+    from Tkinter import filedialog
 import tkinterhtml as tkhtml
 import markdown as mk
 
+def loadfile(filename):
+    try:
+        tmpfile = open(filename,'r')
+        text.delete('1.0', tk.END)
+        text.insert('1.0', tmpfile.read())
+        tmpfile.close()
+    except IOError:
+        tk.messagebox.showerror(title='Error', message='''There's no such file''')
+def savefile(filename):
+    try:
+        tmpfile = open(filename,'w+')
+        tmpfile.write(text.get('1.0', tk.END))
+        tmpfile.close()
+    except IOError:
+        tk.messagebox.showerror(title='Error', message='''File save error''')
 def markdownencoding():
     htmlframe.set_content(mk.markdown(text.get('1.0', tk.END)))
     #text2.insert(tk.INSERT, text.get('1.0', tk.END))
-    
+def encodingbuttoncallback():
+    savefile('tmp.md')
+    markdownencoding()
+def loadtmpbuttoncallback():
+    loadfile('tmp.md')
+    markdownencoding()
+def openfilebuttoncallback():
+    filename = filedialog.askopenfilename()
+    loadfile(filename)
+    markdownencoding()
+def saveasfilebuttoncallback():
+    filename = filedialog.asksaveasfilename()
+    if filename!='':
+        savefile(filename)
+
 # 控件定义
 root = tk.Tk()
 lframe = tk.Frame(root)
+buttonpanel = tk.Frame(lframe)
 s1 = tk.Scrollbar(lframe)
 s2 = tk.Scrollbar(lframe, orient=tk.HORIZONTAL)# HORIZONTAL 设置水平方向的滚动条，默认是竖直
 text = tk.Text(lframe, yscrollcommand=s1.set, xscrollcommand=s2.set, wrap='none')
-button = tk.Button(lframe,text='Encoding', command=markdownencoding)
+buttonencoding = tk.Button(buttonpanel,text='Encoding', command=encodingbuttoncallback)
 htmlframe = tkhtml.HtmlFrame(root)
+buttonloadtmp = tk.Button(buttonpanel, text='Load Tmp', command=loadtmpbuttoncallback)
+buttonopenfile = tk.Button(buttonpanel, text='Open File', command=openfilebuttoncallback)
+buttonsaveasfile = tk.Button(buttonpanel, text='Save As', command=saveasfilebuttoncallback)
 
 # 初始界面
 text.insert(tk.INSERT, '''
@@ -35,7 +70,11 @@ markdownencoding()
 s1.pack(side=tk.RIGHT, fill=tk.Y)
 s2.pack(side=tk.BOTTOM, fill=tk.X)
 text.pack(expand=tk.YES, fill=tk.BOTH)
-button.pack(side=tk.BOTTOM, fill=tk.X)
+buttonencoding.pack(side=tk.RIGHT, fill=tk.X)
+buttonloadtmp.pack(side=tk.LEFT, fill=tk.X)
+buttonopenfile.pack(side=tk.LEFT, fill=tk.X)
+buttonsaveasfile.pack(side=tk.LEFT, fill=tk.X)
+buttonpanel.pack(side=tk.BOTTOM, fill=tk.X)
 lframe.pack(side=tk.LEFT, fill=tk.Y)
 htmlframe.pack(side=tk.RIGHT)
 #text2.pack(side=tk.RIGHT)
